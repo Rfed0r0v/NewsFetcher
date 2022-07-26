e03d6a2970c430ef5546292fa5750b1f7f7d0732
Rfed0r0v <rf.rf944@gmail.com>
2022-07-26 20:51:50 +0300

    конец урока coroutine-1

--- app/src/main/java/com/example/newsfetcher/base/BaseViewModel.kt
+++ app/src/main/java/com/example/newsfetcher/base/BaseViewModel.kt
@@ -2,8 +2,6 @@ package com.example.newsfetcher.base
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.launch
 
 abstract class BaseViewModel<VIEW_STATE> : ViewModel() {
     val viewState: MutableLiveData<VIEW_STATE> by lazy { MutableLiveData(initialViewState()) }
